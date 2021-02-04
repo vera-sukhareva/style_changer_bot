b36c84f27bd5664244ed99cdae8756250c1a4db6
Алеева Вера Александровна <VAAleeva@vimpelcom.ru>
2021-02-04 10:45:17 +0300

    change requirements and pathes

--- transfer_model.py
+++ transfer_model.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
 import torchvision.models as models
 import copy
