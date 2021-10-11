2d1ac9a172be79f5ab11cbf419037f0b26d81ffd
Сухарева Вера Александровна <VASukhareva@vimpelcom.ru>
2021-10-11 18:56:42 +0300

    change token

--- style_bot.py
+++ style_bot.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Done! Congratulations on your new bot. You will find it at t.me/your_style_changer_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
 
-# Use this token to access the HTTP API:
-# 1563972967:AAETCK5ET3MvxQ_tWsISwwyuTB74zMHzk0U
-# Keep your token secure and store it safely, it can be used by anyone to control your bot.
-
-# For a description of the Bot API, see this page: https://core.telegram.org/bots/api
 
 
 import telebot
 from transfer_model import *
 import os
 
-bot = telebot.TeleBot('1563972967:AAETCK5ET3MvxQ_tWsISwwyuTB74zMHzk0U');
+bot = telebot.TeleBot('###');
 
 
 @bot.message_handler(commands=['start'])
